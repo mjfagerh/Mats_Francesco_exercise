2814cb8d83373b8b38da2b9e9614e593d47f5156
mjfagerh <60216159+mjfagerh@users.noreply.github.com>
2022-03-14 22:09:09 +0100

    function for the rules
    created a function that takes in a lenght 3 array and returns the value according to the rules of rule_30.
    also To avoid boundary problems i made the inital matrix a (nxn+2)-matrix. then the plan is to stip away the the first and last column at the end (so a nxn matrix will still be returnes)

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import numpy as np
 
 def inital_state(num_elements):
-    base_matrix = np.zeros((num_elements,num_elements))
+    base_matrix = np.zeros((num_elements, num_elements+2))
     first_pos = int(num_elements/2)
     base_matrix[0, first_pos] = 1
     return base_matrix
+
+def rule_30(control):
+    if (control == np.array([1, 1, 1])).all():
+        return 0
+    elif (control == np.array([1, 1, 0])).all():
+        return 0
+    elif (control == np.array([1, 0, 1])).all():
+        return 0
+    elif (control == np.array([1, 0, 0])).all():
+        return 1
+    elif (control == np.array([0, 1, 1])).all():
+        return 1
+    elif (control == np.array([0, 1, 0])).all():
+        return 1
+    elif (control == np.array([0, 0, 1])).all():
+        return 1
+    elif (control == np.array([0, 0, 1])).all():
+        return 0
+
+
+
+
+
+
+
+
+
+
+

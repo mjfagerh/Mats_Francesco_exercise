a185b1a357d33c8c3c56602225568d5a5b649399
mjfagerh <60216159+mjfagerh@users.noreply.github.com>
2022-03-14 22:47:34 +0100

    error in last commite

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 
 def inital_state(num_elements):
     base_matrix = np.zeros((num_elements, num_elements+2))
-    first_pos = int(num_elements/2)
     base_matrix[0, first_pos] = 1
     return base_matrix
 

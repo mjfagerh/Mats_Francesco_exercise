3c398b0b422c66863423ada343104d31a5d9cbcb
mjfagerh <60216159+mjfagerh@users.noreply.github.com>
2022-03-14 16:13:21 +0100

    Initial base matrix
    created a function that gives the initial base matrix that we want to apply rule 30 on. the matrix is a numpy array, and the inital condition is a 1 in the middle element of the first row

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#test#test ciao hello mats
+import numpy as np
 
-
-
-z=1
+def inital_state(num_elements):
+    base_matrix = np.zeros((num_elements,num_elements))
+    first_pos = int(num_elements/2)
+    base_matrix[0, first_pos] = 1
+    return base_matrix

bc468bcca6d83831c0b34f8d1cb07f13ddf5fcc0
mjfagerh <60216159+mjfagerh@users.noreply.github.com>
2022-03-14 22:46:48 +0100

    creating the main function
    - fixed the inital_stat function, to center the first bit numerated as 1
    
    - created a function to strip away the ends of a matrix (first and last column)
    
    -implemented a error test for the rule_30 function
    
    -the create_matrix iterates through the matrix and updates each row according to rule_30. The returning matrix is a np.array

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def inital_state(num_elements):
     base_matrix = np.zeros((num_elements, num_elements+2))
     first_pos = int(num_elements/2)
     base_matrix[0, first_pos] = 1
     return base_matrix
+
 
 def rule_30(control):
     if (control == np.array([1, 1, 1])).all():
@@ -21,10 +23,26 @@ def rule_30(control):
         return 1
     elif (control == np.array([0, 0, 1])).all():
         return 1
-    elif (control == np.array([0, 0, 1])).all():
+    elif (control == np.array([0, 0, 0])).all():
         return 0
+    else:
+        print("array is not deffined return none")
+        return None
 
 
+def strip_ends(matrix):
+    index = [0, -1]
+    return np.delete(matrix, index, 1)
+
+
+def create_matrix(size):
+    base_matrix = inital_state(size)
+    for row in range(size-1):
+        base_matrix[row]
+        for col in range(1, size):
+            control = base_matrix[row][col-1:col+2]
+            base_matrix[row+1, col] = rule_30(control)
+    return strip_ends(base_matrix)
 
 
 

e33ce7679bd858d3851c9c61a344489c3bc51329
mjfagerh <60216159+mjfagerh@users.noreply.github.com>
2022-03-14 23:07:40 +0100

    added a visualization, and fixing a logical error
    -made a visualtization by using a chess grid
    
    - fized a error that made the main fucntion not update the last row

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def initial_state(num_elements):
@@ -39,10 +40,15 @@ def create_matrix(size):
     base_matrix = initial_state(size)
     for row in range(size-1):
         base_matrix[row]
-        for col in range(1, size):
+        for col in range(1, size+1):
             control = base_matrix[row][col-1:col+2]
             base_matrix[row+1, col] = rule_30(control)
     return strip_ends(base_matrix)
+
+
+def visualize(matrix):
+    plt.matshow(matrix)
+    plt.show()
 
 
 
